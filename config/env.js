89d2c989c6a7cd7d6f2822c8ee1a2f76e1df378d
yubizacomplementos-creator <yubizacomplementos@gmail.com>
2026-04-25 21:49:26 -0600

    Create env.js

---
+++ config/env.js
@@ -0,0 +1,16 @@
+// v1.0 - config entorno
+// avance: 100%
+// líneas: ~25
+
+export const ENV = {
+  VERIFY_TOKEN: process.env.VERIFICAR_TOKEN,
+  WHATSAPP_TOKEN: process.env.WHATSAPP_TOKEN,
+  PHONE_NUMBER_ID: process.env.PHONE_NUMBER_ID,
+
+  GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+  GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+  SHEET_ID: process.env.SHEET_ID,
+
+  SHOPIFY_STORE: process.env.SHOPIFY_STORE,
+  SHOPIFY_TOKEN: process.env.SHOPIFY_TOKEN
+};
